affd1d114a0a323f2df9001a21c92bd458800534
Sarkar <2095491@cognizant.com>
2022-05-18 17:12:18 +0530

    made changes in quotesservice

--- QuotesService/Controllers/QuotesController.cs
+++ QuotesService/Controllers/QuotesController.cs
@@ -11,18 +11,26 @@ namespace QuotesService.Controllers
     [ApiController]
     public class QuotesController : ControllerBase
     {
-        private readonly IQuotesService service;
+        private readonly IQuotesService quotesService;
         static readonly log4net.ILog _log4net = log4net.LogManager.GetLogger(typeof(QuotesController));
         public QuotesController(IQuotesService service)
         {
             
-            this.service = service;
+            this.quotesService = service;
         }
 
+        /// <summary>
+        /// get quotes  for policy
+        /// </summary>
+        /// <param name="businessValue"></param>
+        /// <param name="propertyValue"></param>
+        /// <param name="propertyType"></param>
+        /// <returns></returns>
+   
         [HttpGet("{businessValue}/{propertyValue}/{propertyType}")]
         public IActionResult getQuotesForPolicy(long businessValue,long propertyValue,string propertyType)
         {
-            string quotes= service.GetQuotesForPolicy(businessValue, propertyValue, propertyType);
+            string quotes= quotesService.GetQuotesForPolicy(businessValue, propertyValue, propertyType);
             if (quotes.Length>0)
                 return Ok(new CustomResponse(1,"",quotes));
             return Ok(new CustomResponse(2, "No Quotes, Contact Insurance Provider", null));
